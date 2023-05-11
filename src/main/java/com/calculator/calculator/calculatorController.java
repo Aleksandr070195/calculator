00fbec29bf0ef95b0cfc7ee2ae6471fee6eafcb4
Aleksandr <07_sanek_95@mail.ru>
2023-05-11 16:39:40 +0300

    .

--- src/main/java/com/calculator/calculator/calculatorController.java
+++ src/main/java/com/calculator/calculator/calculatorController.java
@@ -7,26 +7,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class calculatorController {
     @GetMapping("/calculator")
-    public String greating (){
+    public String greating() {
         return "Добро пожаловать в калькулятор";
     }
+
     @GetMapping("/calculator/plus")
-    public String add(@RequestParam int num1, @RequestParam int num2){
-    return num1 + " + " + num2 + " = " + (num1+num2);
+    public String add(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
     }
+
     @GetMapping("/calculator/minus")
-    public String minus(@RequestParam int num1, @RequestParam int num2){
-        return num1 + " - " + num2 + " = " + (num1-num2);
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
     }
+
     @GetMapping("/calculator/multiply")
-    public String multiply(@RequestParam int num1, @RequestParam int num2){
-        return num1 + " * " + num2 + " = " + (num1*num2);
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
     }
+
     @GetMapping("/calculator/divide")
-    public String divide(@RequestParam int num1, @RequestParam int num2){
-        if (num2==0){
-           return  "Делить на 0 нельзя";
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        if (num2 == 0) {
+            return "Делить на 0 нельзя";
         }
-        return num1 + " / " + num2 + " = " + (num1/num2);
+        return num1 + " / " + num2 + " = " + (num1 / num2);
     }
 }
