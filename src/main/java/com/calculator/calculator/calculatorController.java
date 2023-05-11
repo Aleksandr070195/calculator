4b901fc6835412aa866807b211d51db38af08f47
Aleksandr <07_sanek_95@mail.ru>
2023-05-11 16:32:46 +0300

    /

---
+++ src/main/java/com/calculator/calculator/calculatorController.java
@@ -0,0 +1,32 @@
+package com.calculator.calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class calculatorController {
+    @GetMapping("/calculator")
+    public String greating (){
+        return "Добро пожаловать в калькулятор";
+    }
+    @GetMapping("/calculator/plus")
+    public String add(@RequestParam int num1, @RequestParam int num2){
+    return num1 + " + " + num2 + " = " + (num1+num2);
+    }
+    @GetMapping("/calculator/minus")
+    public String minus(@RequestParam int num1, @RequestParam int num2){
+        return num1 + " - " + num2 + " = " + (num1-num2);
+    }
+    @GetMapping("/calculator/multiply")
+    public String multiply(@RequestParam int num1, @RequestParam int num2){
+        return num1 + " * " + num2 + " = " + (num1*num2);
+    }
+    @GetMapping("/calculator/divide")
+    public String divide(@RequestParam int num1, @RequestParam int num2){
+        if (num2==0){
+           return  "Делить на 0 нельзя";
+        }
+        return num1 + " / " + num2 + " = " + (num1/num2);
+    }
+}
